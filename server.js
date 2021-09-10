d7e22d0d6381403ec239fc0cb2df4ed32ec18749
Dinh Chung Nam <dinhchungnam.99@gmail.com>
2021-09-10 18:13:08 +0700

    Auth: Done

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // const env = require('dotenv').config({ path: `${__dirname}/.env` });
 const express = require('express');
 // const morgan = require('morgan');
-// const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 
 const errorHandle = require('./app/middlewares/errorHandle');
 const route = require('./app/routes/route');
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`));
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
 //Route & Middleware
 app.use('/api',route);
