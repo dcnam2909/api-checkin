dc4ff20c9a18215ccd86a6338d1cb7eef3014dd3
Dinh Chung Nam <dinhchungnam.99@gmail.com>
2021-09-18 01:03:00 +0700

    User: Done

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -5,7 +5,7 @@ const checkRoles = require('../middlewares/checkRoles');
 const verifyToken = require('../middlewares/verifyToken');
 
 userRoute.get('/', verifyToken, userController.getInfo);
-userRoute.put('/', verifyToken, userController.updateInfo);
-userRoute.patch('/changePass', verifyToken, userController.changePassword);
+userRoute.patch('/update', verifyToken, userController.updateInfo);
+userRoute.patch('/changePassword', verifyToken, userController.changePassword);
 
 module.exports = userRoute;
