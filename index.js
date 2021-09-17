c19f85e06df3952b8a7fe63983c92a1418f0b4c3
Dinh Chung Nam <dinhchungnam.99@gmail.com>
2021-09-17 22:16:51 +0700

    change pass route: error

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-// const env = require('dotenv').config({ path: `${__dirname}/.env` });
-// const morgan = require('morgan');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-// app.use(morgan('dev'));
 const errorHandle = require('./app/middlewares/errorHandle');
 const route = require('./app/routes/route');
 
