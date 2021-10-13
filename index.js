d93a5dadec389dfc8de0fd356b04e1052111a8ab
Dinh Chung Nam <dinhchungnam.99@gmail.com>
2021-10-13 21:41:24 +0700

    Test CORS

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
 
 const errorHandle = require('./app/middlewares/errorHandle');
 const route = require('./app/routes/route');
 const db = require('./app/config/db')
 //Middleware global
-// app.use(cors);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,6 +14,7 @@ app.use(bodyParser.json());
 //Route
 app.use('/api', route);
 app.use(errorHandle);
+app.use(cors);
 
 //Database connect
 // db();
